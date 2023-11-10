f28fc9884b745778a9fbafa8d1b6bc32de5e83d1
Kushtar <Amalbekov312@gmail.com>
2023-11-10 18:58:34 +0600

    Added project

---
+++ src/main/java/com/gtp/kushtar/KushtarApplication.java
@@ -0,0 +1,13 @@
+package com.gtp.kushtar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KushtarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KushtarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtp/kushtar/KushtarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtp.kushtar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KushtarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
